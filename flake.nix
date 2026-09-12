ec1cf8c81c80f8b38cb43c4e3b13d5d4f555cb7e
tryy3 <1096762+tryy3@users.noreply.github.com>
2026-09-12 11:29:02 +0000

    chore(update): helium to 0.17.0.1

--- flake.nix
+++ flake.nix
@@ -26,11 +26,11 @@
         helium = pkgs.appimageTools.wrapType2 rec {
 
           pname = "helium";
-          version = "0.16.6.1";
+          version = "0.17.0.1";
 
           src = pkgs.fetchurl {
             url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-            sha256 = "sha256-T29e5QpXsFYADvSsNcti2LXqLaCUjB5mLYEnHtpxO/Q=";
+            sha256 = "sha256-JmqdEwoXP/2GGAMS2gjAq7G2oWFuyUTr5ouMDhSOcHY=";
           };
 
           extraInstallCommands =
